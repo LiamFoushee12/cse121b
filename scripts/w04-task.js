f718e39407957b528c917ba5ca98c2c941f2c27f
LiamFoushee12 <80227989+LiamFoushee12@users.noreply.github.com>
2024-02-14 16:58:45 -0700

    started w04

---
+++ scripts/w04-task.js
@@ -0,0 +1,71 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Liam Foushee",
+    photo: "images/profile.jpg",
+    favoriteFoods: [
+        "Pizza", 
+        "Sushi", 
+        "Pasta"
+        ],
+    hobbies: ["Gaming", "Reading", "Coding"],
+    placesLived: []
+}
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: "Portland, OR",
+        length: "10 years"
+    },
+    {
+        place: "Gilbert, Az",
+        length: "4 years"
+    },
+    {
+        place: "LaCenter, WA",
+        length: "4 years"
+    },
+    {
+        place: "Rexburg, ID",
+        length: "3 years"
+    }
+  );
+
+/* DOM Manipulation - Output */
+document.querySelector('#name').textContent = myProfile.name;
+
+/* Name */
+//document.querySelector("#name").textContent = myProfile.name;
+
+/* Photo with attributes */
+document.querySelector("#photo").src = myProfile.photo;
+
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favoriteFoods').appendChild(li);
+});
+
+/* Hobbies List */
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement('li');
+    li.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(li);
+});
+
+/* Places Lived DataList */
+var dlElement = document.querySelector("#places-lived");
+
+myProfile.placesLived.forEach(place => {
+    var dtElement = document.createElement("dt");
+    dtElement.textContent = place.place;
+    var ddElement = document.createElement("dd");
+    ddElement.textContent = place.length;
+    dlElement.appendChild(dtElement);
+    dlElement.appendChild(ddElement);
+});
+
